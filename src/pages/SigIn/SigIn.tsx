7d4309787ef116d96fc41465a11b8caa970c2b5f
Alexey <villas1998@mail.ru>
2019-09-01 02:48:28 +0200

    валдация с япом

--- src/pages/SigIn/SigIn.tsx
+++ src/pages/SigIn/SigIn.tsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import Wrapper from './SigIn.style';
+import * as Yup from 'yup';
 
 export interface SigInProps {
   children: React.ReactNode;
@@ -8,11 +9,32 @@ export interface SigInProps {
 class SigIn extends PureComponent<SigInProps> {
   public static defaultProps: Partial<SigInProps> = {
     children: null
+const SigInSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Слишком короткое имя!')
+    .max(50, 'Слишком Длинное имя!')
+    .required('Обезательное поле!'),
+  lastName: Yup.string()
+    .min(2, 'Слишком короткая фамилия!')
+    .max(50, 'Слишком длинная фамилия!')
+    .required('Обезательное поле!'),
+  nickName: Yup.string()
+    .min(2, 'Слишком короткий ник!')
+    .max(50, 'Слишком длинный ник!'),
+  email: Yup.string()
+    .min(2, 'Слишком короткое имя!')
+    .max(50, 'Слишком Длинное имя!'),
+  password: Yup.string()
+    .min(2, 'Слишком короткое имя!')
+    .max(50, 'Слишком Длинное имя!')
+    .required('Обезательное поле!')
+});
   };
 
-  public render() {
-    return <Wrapper>dasdasdasdasdasd</Wrapper>;
-  }
-}
+  const renderForm = (formikBag: FormikProps<FormValues>) => (
+    <Form>
+      <Field name="firstName" />
+      <ErrorMessage name="firstName" />
+      validationSchema={SigInSchema}
 
 export default SigIn;
