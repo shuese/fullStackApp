8bd2dc811fe1348c0dc236b854f402d385644407
Kafkae <dark.revo4@mail.ru>
2020-05-15 14:51:48 +0400

    убрал хрень

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -15,12 +15,6 @@ router.post("/signup", (req: any, res: any) => {
 
   user.save((errors: any) => {
     if (errors) {
-      if (errors?.errors?.email?.properties?.type) {
-        errors.errors.email.message = "Эта почта уже занята!";
-      }
-      if (errors?.errors?.nickName?.properties?.type) {
-        errors.errors.nickName.message = "Этот ник уже занят!";
-      }
       return res.status(404).send({ ...errors });
     }
     return res.send({ message: "succes" });
